fcae2308144f5fe045aad5bd2ed77e989bf17976
rahul3355 <rahulpatil33553@gmail.com>
2022-09-15 16:12:19 +0530

    editLeave deleteLeave UI

--- src/Component/DeleteLeave.js
+++ src/Component/DeleteLeave.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import moment from "moment";
+import './EditLeaveMan.css';
 
 const DeleteLeave = () => {
     const [submit, setSubmit] = useState(null);
     const [inputs, setInputs] = useState({});
+    const navigate = useNavigate();
 
     const LeaveId = localStorage.getItem('LID');
     const EmployeeId = localStorage.getItem('EID');
@@ -17,6 +29,29 @@ const DeleteLeave = () => {
 
     let LeaveStart1 = LeaveStart.slice(0, 10);
     let LeaveEnd1 = LeaveEnd.slice(0, 10);
+
+    var start = moment(LeaveStart);
+    var end = moment(LeaveEnd);
+
+
+
+    const getBusinessDatesCount = (startDate, endDate) => {
+        let count = 0;
+        let curDate = +startDate;
+        while (curDate <= +endDate) {
+            const dayOfWeek = new Date(curDate).getDay();
+            const isWeekend = (dayOfWeek === 6) || (dayOfWeek === 0);
+            if (!isWeekend) {
+                count++;
+            }
+            curDate = curDate + 24 * 60 * 60 * 1000
+        }
+        return count;
+    }
+
+    var diff2 = getBusinessDatesCount(start, end)
+    console.log(diff2)
+    const leaveDays = diff2;
 
     const handleChange = (event) => {
         const name = event.target.id;
@@ -34,63 +69,105 @@ const DeleteLeave = () => {
         fetch('http://localhost:12242/api/Leavelms/' + LeaveId, requestOptions)
             .then(response => response.json())
             .then(setSubmit(1));
+        alert("Leave request deleted");
+        navigate('/AllLeave');
     }
 
     return (
-        <div>
+        <div class="container-xl">
             <form onSubmit={handleSubmit}>
-                <br/>
-                <h2>DELETE LEAVE RECORD</h2>
-                <br/><br/>
-                
-                <dl class="row">
-                    <dt class="col-sm-9">Leave ID</dt>
-                    <dd class="col-sm-1">{LeaveId}</dd>
-                </dl>
-                <dl class="row">
-                    <dt class="col-sm-9">Employee ID</dt>
-                    <dd class="col-sm-1">{EmployeeId}</dd>
-                </dl>
-                <dl class="row">
-                    <dt class="col-sm-9">Employee Level</dt>
-                    <dd class="col-sm-1">{EmplLevel}</dd>
-                </dl>
-                <dl class="row">
-                    <dt class="col-sm-9">Manager ID</dt>
-                    <dd class="col-sm-1">{ManagerId}</dd>
-                </dl>
-                <dl class="row">
-                    <dt class="col-sm-9">Leaves in hand</dt>
-                    <dd class="col-sm-1">{LeavesInHand}</dd>
-                </dl>
-                <blockquote class="blockquote">
-                    <dl class="row">
-                        <mark>
-                            <p><strong>Leave Start : {LeaveStart1}</strong></p></mark>
-                    </dl>
-                    <dl class="row"><mark>
-                        <p><strong>Leave End : {LeaveEnd1}</strong></p></mark>
-                    </dl>
-                </blockquote>
-
-                <dl class="row">
-
-                    <dt class="col-sm-9">Leave Type: </dt>
-                    <dd class="col-sm-3">{LeaveType}</dd>
-                </dl>
+                <div class="container-fluid">
+                <br /> <br /> 
+                    <h1 class="display-6" id= "headLeave">DELETE LEAVE REQUEST</h1>
 
 
-                <dl class="row">
-                    <dt class="col-sm-9">Reason: </dt>
-                    <dd class="col-sm-3">{Reason}</dd>
-                </dl>
+                    <br />
 
-                {/* <input type="submit"></input> */}
-                <button type="submit" class="btn btn-danger">DELETE</button>
-                <br />
-                {submit &&
-                    <label>Leave record deleted</label>
-                }
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+
+                            <TableBody>
+
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leave ID
+                                    </TableCell>
+                                    <TableCell align="left">{LeaveId}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Employee ID
+                                    </TableCell>
+                                    <TableCell align="left">{EmployeeId}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Employee Level
+                                    </TableCell>
+                                    <TableCell align="left">{EmplLevel}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Manager ID
+                                    </TableCell>
+                                    <TableCell align="left">{ManagerId}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leaves in hand
+                                    </TableCell>
+                                    <TableCell align="left">{LeavesInHand}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leave Days
+                                    </TableCell>
+                                    <TableCell align="left">{leaveDays} Days</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leave Start
+                                    </TableCell>
+                                    <TableCell align="left" id="tableStart">{LeaveStart1}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leave End
+                                    </TableCell>
+                                    <TableCell align="left" id="tableStart">{LeaveEnd1}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Leave Type
+                                    </TableCell>
+                                    <TableCell align="left">{LeaveType}</TableCell>
+
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Reason
+                                    </TableCell>
+                                    <TableCell align="left" id="tableReason">{Reason}</TableCell>
+
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    {/* <input type="submit"></input> */}
+                    <button type="submit" class="btn btn-danger">DELETE</button>
+                    <br /><br /> <br /> <br /> <br /><br /> <br /> <br /> <br />
+                    <p>-</p>
+                </div>
             </form>
         </div>
     );
--- src/Component/EditLeaveMan.js
+++ src/Component/EditLeaveMan.js
@@ -4,6 +4,30 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './AddEmp.css';
 import './EditLeaveMan.css';
 import moment from "moment";
+import Button from '@mui/material/Button';
+/* import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { borderLeftColor } from "@mui/system"; */
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 
@@ -30,33 +54,39 @@ const EditLeaveMan = (props) => {
 
     var start = moment(LeaveStart);
     var end = moment(LeaveEnd);
-    
-    
+
+
 
     const getBusinessDatesCount = (startDate, endDate) => {
         let count = 0;
         let curDate = +startDate;
         while (curDate <= +endDate) {
-          const dayOfWeek = new Date(curDate).getDay();
-          const isWeekend = (dayOfWeek === 6) || (dayOfWeek === 0);
-          if (!isWeekend) {
-            count++;
-          }
-          curDate = curDate + 24 * 60 * 60 * 1000
+            const dayOfWeek = new Date(curDate).getDay();
+            const isWeekend = (dayOfWeek === 6) || (dayOfWeek === 0);
+            if (!isWeekend) {
+                count++;
+            }
+            curDate = curDate + 24 * 60 * 60 * 1000
         }
         return count;
-      }
-    
+    }
+
     var diff2 = getBusinessDatesCount(start, end)
     console.log(diff2)
     const leaveDays = diff2;
 
-    
+
     const handleChange = (event) => {
         const name = event.target.id;
         const value = event.target.value;
         setInputs(values => ({ ...values, [name]: value }))
     }
+
+/*     const Demo = styled('div')(({ theme }) => ({
+        backgroundColor: theme.palette.background.paper
+       
+    }));
+ */
 
 
 
@@ -125,79 +155,116 @@ const EditLeaveMan = (props) => {
 
     return (
         <div>
+            <br />
             <div class="container-lg">
-                
+            
                 <div class="jumbotron jumbotron-fluid">
-                    <div class="container-xl">
-                        <h1 class="display-4">Review Leave Request</h1>
-                    
+                
+                    <div class="container-fluid">
 
-                <br />
-
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
-                <form>
+                        <h1 class="display-6" id="head1">REVIEW LEAVE REQUEST</h1>
 
 
+                       
 
-
-                    <dl class="row">
-                        <dt class="col-sm-9">Leave ID</dt>
-                        <dd class="col-sm-1">{LeaveId}</dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-9">Employee ID</dt>
-                        <dd class="col-sm-1">{EmployeeId}</dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-9">Employee Level</dt>
-                        <dd class="col-sm-1">{EmplLevel}</dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-9">Manager ID</dt>
-                        <dd class="col-sm-1">{ManagerId}</dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-9">Leaves in hand</dt>
-                        <dd class="col-sm-1">{LeavesInHand}</dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-9" id="leaveDays">Leave Days</dt>
-                        <dd class="col-sm-2" id="leaveDays1">{leaveDays} Days</dd>
-                    </dl>
-                    <blockquote class="blockquote">
-                        <dl class="row">
-                            <mark>
-                                <p><strong>Leave Start : {LeaveStart1}</strong></p></mark>
-                        </dl>
-                        <dl class="row"><mark>
-                            <p><strong>Leave End : {LeaveEnd1}</strong></p></mark>
-                        </dl>
-                    </blockquote>
-
-                    <dl class="row">
-
-                        <dt class="col-sm-9">Leave Type: </dt>
-                        <dd class="col-sm-3">{LeaveType}</dd>
-                    </dl>
-
-
-                    <dl class="row">
-                        <dt class="col-sm-9">Reason: </dt>
-                        <dd class="col-sm-3">{Reason}</dd>
-                    </dl>
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+                        <form>
 
 
 
-                    <button type="submit" onClick={handleSubmitApprove} class="btn btn-success">APPROVE</button>
+                           <Typography>
+                               
 
-                    <button type="submit" onClick={handleSubmitReject} class="btn btn-danger">REJECT</button>
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        
+        <TableBody>
+          
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Leave ID
+              </TableCell>
+              <TableCell align="left">{LeaveId}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Employee ID
+              </TableCell>
+              <TableCell align="left">{EmployeeId}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Employee Level
+              </TableCell>
+              <TableCell align="left">{EmplLevel}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Manager ID
+              </TableCell>
+              <TableCell align="left">{ManagerId}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Leaves in hand
+              </TableCell>
+              <TableCell align="left">{LeavesInHand}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Leave Days
+              </TableCell>
+              <TableCell align="left">{leaveDays} Days</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Leave Start
+              </TableCell>
+              <TableCell align="left" id="tableStart">{LeaveStart1}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Leave End
+              </TableCell>
+              <TableCell align="left" id="tableStart">{LeaveEnd1}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              Leave Type
+              </TableCell>
+              <TableCell align="left">{LeaveType}</TableCell>
+      
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                Reason
+              </TableCell>
+              <TableCell align="left" id="tableReason">{Reason}</TableCell>
+      
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+                                <br/><br/>
 
+                                <Button type="submit" onClick={handleSubmitApprove} class="btn btn-success">APPROVE</Button>
 
-                </form>
+                                <Button type="submit" onClick={handleSubmitReject} class="btn btn-danger">REJECT</Button>
 
-            </div>
-            </div>
+                            </Typography>
+                        </form>
+
+                    </div>
                 </div>
+            </div>
 
         </div>
 
